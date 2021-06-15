2c0f5a0cc4e5cacfcae8a499e41ad7a6431527c6
alex-mtx <alexander.molinari@gmail.com>
2021-06-16 00:24:03 +0200

    Including EF Core

--- Mtx.Common.Domain/Entity.cs
+++ Mtx.Common.Domain/Entity.cs
@@ -5,13 +5,22 @@ using MediatR;
 
 namespace Mtx.Common.Domain
 {
-    public abstract class Entity
+    public abstract class Entity : IEntity<Guid>
     {
         private List<INotification> _domainEvents = new();
 
         public Guid Id { get; private set; } = Guid.NewGuid();
 
         public List<INotification> DomainEvents => _domainEvents;
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? ModifiedBy { get; set; }
+
+        object IEntity.Id { get => this.Id; }
+
+        Guid IEntity<Guid>.Id { get; } = Guid.NewGuid();
 
         public void AddDomainEvent(INotification eventItem)
 
@@ -30,6 +39,20 @@ namespace Mtx.Common.Domain
             _domainEvents.Remove(eventItem);
 
         }
+    }
+
+    public interface IEntity 
+    {
+        object Id { get; }
+        DateTime CreatedDate { get; set; }
+        DateTime? ModifiedDate { get; set; }
+        string? CreatedBy { get; set; }
+        string? ModifiedBy { get; set; }
+    }
+
+    public interface IEntity<T> : IEntity
+    {
+        new T Id { get;  }
     }
 
 }
---
+++ Services/Mtx.EFCoreServices/EFCoreRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mtx.Common.Domain;
+using Mtx.Common.Domain.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace Mtx.EFCoreServices
+{
+    public class EFCoreRepository<TContext> : EFCoreReadOnlyRepository<TContext>, IRepository
+      where TContext : DbContext
+    {
+        public EFCoreRepository(TContext context)
+            : base(context)
+        {
+        }
+
+        public virtual void Create<TEntity>(TEntity entity, string? createdBy = null)
+            where TEntity : class, IEntity
+        {
+            entity.CreatedDate = DateTime.UtcNow;
+            entity.CreatedBy = createdBy;
+            context.Set<TEntity>().Add(entity);
+        }
+
+        public virtual void Update<TEntity>(TEntity entity, string? modifiedBy = null)
+            where TEntity : class, IEntity
+        {
+            entity.ModifiedDate = DateTime.UtcNow;
+            entity.ModifiedBy = modifiedBy;
+            context.Set<TEntity>().Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete<TEntity>(object id)
+            where TEntity : class, IEntity
+        {
+            TEntity entity = context.Set<TEntity>().Find(id);
+            Delete(entity);
+        }
+
+        public virtual void Delete<TEntity>(TEntity entity)
+            where TEntity : class, IEntity
+        {
+            var dbSet = context.Set<TEntity>();
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+
+        public virtual void Save()
+        {
+                context.SaveChanges();
+        }
+
+        public virtual Task SaveAsync()
+        {
+                return context.SaveChangesAsync();
+        }
+
+    }
+}
