38045923179b492f8722aa175f8d71df38d3d33f
alex mtx <alexander.molinari@gmail.com>
2021-11-04 02:13:45 +0100

    Added Orleans Hosting

---
+++ Hosting/Orleans/Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using Grains;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Mtx.LearnItAll.Core.Common;
+using Mtx.LearnItAll.Core.Common.Parts;
+using Orleans;
+using Orleans.Clustering.CosmosDB;
+using Orleans.Hosting;
+using Spectre.Console;
+using System;
+using System.Threading.Tasks;
+
+var client = new ClientBuilder()
+    .ConfigureAppConfiguration((host, builder) =>
+    {
+        builder.AddEnvironmentVariables();
+        if ("Development".Equals(host.HostingEnvironment.EnvironmentName))
+        {
+            builder.AddUserSecrets<Program>();
+        }
+    })
+    .UseLocalhostClustering()
+    .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ISkillBlueprintGrain).Assembly).WithReferences())
+    .UseCosmosDBGatewayListProvider(x =>
+    {
+
+        x.AccountEndpoint = "https://learn-it-all-cosmos-account-dev.documents.azure.com:443/";
+        x.AccountKey = "LubXYtKazoKDbTOVeWp3O2JgmckNXOKOCGZFkdRZ2wfkSKDq7OSyrQu8SIyQVgfRa0zhOOhV9zqIViGB7oVKoQ==";
+
+
+    })
+    .UseEnvironment("Development")
+    .Build();
+
+await AnsiConsole.Status().StartAsync("Connecting to server", async ctx =>
+{
+    ctx.Spinner(Spinner.Known.Dots);
+    ctx.Status = "Connecting...";
+
+    await client.Connect(async error =>
+    {
+        AnsiConsole.MarkupLine("[bold red]Error:[/] error connecting to server!");
+        AnsiConsole.WriteException(error);
+        ctx.Status = "Waiting to retry...";
+        await Task.Delay(TimeSpan.FromSeconds(2));
+        ctx.Status = "Retrying connection...";
+        return true;
+    });
+
+    ctx.Status = "Connected!";
+});
+
+try
+{
+    var userName = AnsiConsole.Ask<string>("What is the [aqua]name[/] of the Skill?");
+    var grain = client.GetGrain<ISkillBlueprintGrain>(userName);
+    await grain.Add(new Mtx.LearnItAll.Core.Blueprints.PartNode(new Name("if")));
+    //grain.AddPart(newPart: new AddPartCmd(new Name("string"), grain.SkillId));
+}
+catch (Exception e)
+{
+
+    AnsiConsole.WriteException(e);
+    Console.Read();
+}
+finally
+{
+    await client.Close();
+
+}
---
+++ OrleansServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using Grains;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+
+await Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration((hostContext, builder) =>
+    {
+        builder.AddEnvironmentVariables();
+        if (hostContext.HostingEnvironment.IsDevelopment())
+        {
+            builder.AddUserSecrets<Program>();
+        }
+    })
+
+#if DEBUG
+        .UseEnvironment("Development") 
+#endif
+    .UseOrleans((host, silo) =>
+    {
+        silo
+            .UseLocalhostClustering()
+            .ConfigureApplicationParts(parts => parts
+            .AddApplicationPart(typeof(ISkillBlueprintGrain).Assembly)
+            .AddApplicationPart(typeof(SkillBlueprintGrain).Assembly)
+            )
+           .UseCosmosDBMembership(x =>
+           {
+               x.AccountEndpoint = host.Configuration["CosmosDBClusteringOptions:AccountEndpoint"];
+               x.AccountKey = host.Configuration["CosmosDBClusteringOptions:AccountKey"];
+               x.CanCreateResources = true;
+
+           })
+           .AddCosmosDBGrainStorageAsDefault(x =>
+           {
+               x.AccountKey = host.Configuration["CosmosDBStorageOptions:AccountKey"];
+               x.AccountEndpoint = host.Configuration["CosmosDBStorageOptions:AccountEndpoint"];
+
+           }).AddCosmosDBGrainStorage("skillBlueprintState",x =>
+           {
+               x.AccountKey = host.Configuration["CosmosDBStorageOptions:AccountKey"];
+               x.AccountEndpoint = host.Configuration["CosmosDBStorageOptions:AccountEndpoint"];
+           })
+           ;
+           //.AddCosmosDBGrainStorage(skillBlueprintState)
+            //.UseCosmosDBReminderService(op =>
+            //{
+            //    op.DB = "";
+            //    // Configure CosmosDB reminders provider.
+            //    // Configure CosmosDB settings in opt.
+            //})
+    })
+    .RunConsoleAsync();
