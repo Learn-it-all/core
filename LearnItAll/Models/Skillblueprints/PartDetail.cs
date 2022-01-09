921715bd06e6461e69be05bf2ed85ec739a95e25
alex mtx <alexander.molinari@gmail.com>
2022-01-09 23:55:54 +0100

    Responsive UI

--- LearnItAll/Models/Skillblueprints/PartDetail.cs
+++ LearnItAll/Models/Skillblueprints/PartDetail.cs
@@ -8,6 +8,15 @@ namespace LearnItAll.Models.Skillblueprints
         public Guid BlueprintId { get; set; }
         public virtual bool IsRoot => false;
         public virtual int IdentationLevel { get; set; } = 1;
+        public virtual string PartialName(int length = 10, string suffix = "...")
+        {
+            var nameLength = Part.Name.Length;
+            var suffixLength = suffix.Length;
+            var totalLength = length + suffixLength;
+            if (nameLength <= totalLength) return Part.Name;
+            return Part.Name.Substring(0,length) + suffix;
+
+        }
     }
 
     public class RootPartDetail : PartDetail
