ac95c0dfcf1c43b069ca6a324c52d390369c64e7
Alex Mtx <alex-mtx@users.noreply.github.com>
2023-03-22 18:08:42 +0100

    update

--- Hosting/Mtx.LearnItAll.Core.API.Serverless.Azure/Infrastructure/Cosmos/CosmosConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mtx.LearnItAll.Core.API.Serverless.Azure.Infrastructure.Cosmos
-{
-   public class CosmosConfig
-    {
-        public string Key { get; set; } = string.Empty;
-        public string Endpoint { get; set; } = string.Empty;
-        public string DbName { get;  set; } = string.Empty;
-    }
-}
